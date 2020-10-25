b807a1d6c6a55bb236e13109f9558cee28b35676
nathanielik <nathaniel_ik@yahoo.com>
2020-10-25 19:49:37 -0400

    Cookies

---
+++ models/promotion.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+require('mongoose-currency').loadType(mongoose);
+const Currency = mongoose.Types.Currency;
+const Schema = mongoose.Schema;
+
+const promotionSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    featured: {
+        type: Boolean,
+        default: false
+    },
+    cost: {
+        type: Currency,
+        default: false
+    },
+}, {
+    timestamps: true
+});
+const Promotion = mongoose.model('Promotion', promotionSchema);
+
+module.exports = Promotion;
